d04abbf7291ce96cd7ec4bd8d73176b1d6fbba57
jugglingdev <kaylamarie1785@gmail.com>
2024-01-22 18:43:42 -0600

    Complete Exercise 7

---
+++ exercise-7.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Sales (
+    SaleID int,
+    ProductID int,
+    SaleAmount float
+);
+
+INSERT INTO Sales
+VALUES (1, 1, 1200.00), 
+       (2, 2, 250.50);
+
+SELECT SUM(SaleAmount)
+FROM Sales;
